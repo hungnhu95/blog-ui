9741006ead68ea20c5c9f15bb25849b8af67cc07
hungnhu95 <ngaydeptuoi45@gmail.com>
2022-07-29 17:33:59 +0700

    update routes

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// không cần đăng nhập vẫn xem đc
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+// cần đăng nhập mới xem được
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
